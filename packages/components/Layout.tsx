05e88bbeccc6ccd558f2d935af917ae7b03f74f2
Raysson Ornelas <rayssonornelas2006@gmail.com>
2022-09-19 20:56:16 -0300

    fea: login page layout creation

--- packages/components/Layout.tsx
+++ packages/components/Layout.tsx
@@ -9,7 +9,7 @@ function Layout(props: Props) {
   const { children } = props;
 
   return (
-    <div className='min-h-screen bg-blue-gray-900 '>
+    <div className='bg-gradient-to-r from-blue-gray-900 to-blue-gray-500'>
       <Container>
         <div className='flex flex-col justify-center'>{children}</div>
       </Container>
