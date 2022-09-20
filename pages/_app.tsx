f65c87ff4bb86bc76dc064e244f3979f7c708a94
Raysson Ornelas <rayssonornelas2006@gmail.com>
2022-09-20 14:28:16 -0300

    fea: next auth installation and configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,26 @@
 import '../styles/globals.css';
 import { ThemeProvider } from '@material-tailwind/react';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 
 import Layout from '../packages/components/Layout';
 
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) {
   return (
-    <ThemeProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
