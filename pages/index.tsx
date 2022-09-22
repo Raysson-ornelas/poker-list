3edca7c32a3a76a08caf2e71ddc5b2ef2701c84d
Raysson Ornelas <rayssonornelas2006@gmail.com>
2022-09-22 20:45:07 -0300

    fix: index.tsx submission

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,37 @@
 import { getSession } from 'next-auth/react';
 import { Button, Input } from '@material-tailwind/react';
+import { PrismaClient } from '@prisma/client';
 
-import NavBar from '../packages/components/NavBar';
+import Grid from '../packages/components/Grid';
+import Navbar from '../packages/components/NavBar';
 
-import type { GetServerSideProps, NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
 
-const Home: NextPage = () => {
+export default function Home({ initialTournaments = [] }) {
   return (
     <>
-      <NavBar />
+      <Navbar />
       <div className='flex flex-col lg:flex-row gap-4 w-full my-8'>
         <Input label='Torneio' />
         <Input label='Data' />
         <Input label='Buy-in' />
         <Button className='lg:w-56'>Salvar</Button>
       </div>
+      <Grid tournaments={initialTournaments} />
     </>
   );
-};
+}
+
+const prisma = new PrismaClient();
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
+  const tournaments = await prisma.tournaments.findMany();
+  console.log(tournaments);
 
   if (!session) {
     return {
+      props: {},
       redirect: {
         destination: '/login',
         permanent: false,
@@ -33,9 +41,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   return {
     props: {
+      initialTournaments: JSON.parse(JSON.stringify(tournaments)),
       session,
     },
   };
 };
-
-export default Home;
